bf86304b78fc73076c58a943f501b3e9fd5115d8
Hesbon <cityalight.hesbon@gmail.com>
2025-09-07 11:27:20 +0100

    Update middleware.py

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,6 +1,7 @@
 from django.utils.deprecation import MiddlewareMixin
 from ipware import get_client_ip
-from .models import RequestLog
+from django.http import HttpResponseForbidden
+from .models import RequestLog, BlockedIP
 
 
 class LogRequestDetailsMiddleware(MiddlewareMixin):
@@ -8,8 +9,12 @@ class LogRequestDetailsMiddleware(MiddlewareMixin):
         ip, _ = get_client_ip(request)
         ip_address = ip or "0.0.0.0"
 
+        if BlockedIP.objects.filter(ip_address=ip_address).exists():
+            return HttpResponseForbidden("Access denied")
+
         RequestLog.objects.create(
             ip_address=ip_address,
             path=request.path,
         )
+
         return None
